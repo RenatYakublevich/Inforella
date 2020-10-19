6c03cb2cd0a490acedf81a3393041aeb4ae0c397
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-10-19 13:24:32 +0300

    alpha 1.1
    add count files

--- inforella.py
+++ inforella.py
@@ -5,29 +5,26 @@ import argparse
 cli = argparse.ArgumentParser(description='Inforella')
 cli.add_argument("--dir", default='.', type=str, help="Директория для сканирования")
 
-args = cli.parse_args()
 
-all_file = [file for file in os.listdir(args.dir)]
-count_files = len(all_file)
+def is_cache_file(name_file : str) -> bool:
+	""" Проверяет, является ли файл кешом Python интерпритатора """
+	return '.pyc' == name_file[-4::]
 
-count_lines_code = 0
+try:
+	args = cli.parse_args()
+	count_lines_code, count_def_code, count_files = 0, 0, 0
+	tree = os.walk(args.dir)
+	for files in tree:
+		for file in files[2]:
+			try:
+				if not is_cache_file(file):
+					count_files += 1
+				count_lines_code += sum(1 for line in open(files[0] + '/' + file, encoding='utf-8'))
+			except UnicodeDecodeError:
+				pass
 
-tree = os.walk(args.dir)
+	print(f'Количество строк кода - {count_lines_code}')
+	print(f'Количество файлов в проекте - {count_files}')
 
-for files in tree:
-	for file in files[2]:
-		try:
-			count_lines_code += sum(1 for line in open(files[0] + '/' + file,encoding='utf-8'))
-		except UnicodeDecodeError:
-			pass
-
-		
-	
-
-print(f'Количество строк кода - {count_lines_code}')
-
-
-
-
-
-
+except FileNotFoundError:
+	print('Не удалось найти директорию')
