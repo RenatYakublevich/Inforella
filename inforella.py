1c7fc062f2e1cc6c33bf4f9430cd881d4c9fa575
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-10-19 22:58:35 +0300

    alpha 1.3
    add count comments

--- inforella.py
+++ inforella.py
@@ -1,47 +1,56 @@
 import os 
 import argparse
 import re
+from termcolor import colored
+from colorama import Fore, Back, Style
 
 
 cli = argparse.ArgumentParser(description='Inforella')
 cli.add_argument("--dir", default='.', type=str, help="Директория для сканирования")
 
 args = cli.parse_args()
-count_lines_code, count_def_code, count_files = 0, 0, 0
+count_lines_code, count_def_code, count_files, count_comments_code = 0, 0, 0, 0
+# дерево файлов
 tree = os.walk(args.dir)
 
 
 def is_cache_file(name_file: str) -> bool:
-	""" Проверяет, является ли файл кешом Python интерпритатора """
-	return '.pyc' == name_file[-4::]
+    """ Проверяет, является ли файл кешом Python интерпритатора """
+    return '.pyc' == name_file[-4::]
 
 
-def count_function_file(name_file: str) -> int:
-	count_def = 0
-	with open(name_file, encoding='utf-8') as file_func:
-		for line in file_func:
-			count_def += len(re.findall(r'def', line))
+def count_word_file(name_file: str, word: str) -> int:
+    """ Возвращает количество определённых слов в файле """
+    count_words = 0
+    with open(name_file, encoding='utf-8') as file_func:
+        for line in file_func:
+            count_words += len(re.findall(word, line))
 
-	return count_def
+    return count_words
 
 
 try:
-	for files in tree:
-		for file in files[2]:
-			try:
-				# если файл не кеш
-				if not is_cache_file(file):
-					count_files += 1
-				# считаем кол-во строк в файле
-				count_lines_code += sum(1 for line in open(files[0] + '/' + file, encoding='utf-8'))
-				count_def_code += count_function_file(files[0] + '/' + file)
-			except UnicodeDecodeError:
-				pass
-	print(f'Количество строк кода - {count_lines_code}\n')
-	print(f'Количество файлов в проекте - {count_files}\n')
-	print(f'Количество функций в проекте - {count_def_code}\n')
+    for files in tree:
+        for file in files[2]:
+            try:
+                # если файл не кеш
+                if not is_cache_file(file):
+                    count_files += 1
+                # считаем кол-во строк в файле
+                count_lines_code += sum(1 for line in open(files[0] + '/' + file, encoding='utf-8'))
+                # считаем количество функций в файле
+                count_def_code += count_word_file(files[0] + '/' + file, 'def')
+                # считаем количество комментарий в файле
+                count_comments_code += count_word_file(files[0] + '/' + file, '#')
+            except UnicodeDecodeError:
+                pass
+    print(f'Количество строк кода - {count_lines_code}\n')
+    print(f'Количество файлов в проекте - {count_files}\n')
+    print(f'Количество функций в проекте - {count_def_code}\n')
+    print(f'Количество комментарий в проекте - {count_comments_code}')
 
 except FileNotFoundError:
-	print('Не удалось найти директорию')
+    print('Не удалось найти директорию')
+
 except Exception:
-	print('Упс...\nЧто-то пошло не так')
+    print('Упс...\nЧто-то пошло не так')
